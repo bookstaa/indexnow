c5a632c4430bd8f7faef6409d3eacc108997d835
bookstaa <bookstaaofficial@gmail.com>
2025-07-22 17:48:54 +0530

    Update indexnow-key.js

--- api/indexnow-key.js
+++ api/indexnow-key.js
@@ -1,1 +1,7 @@
+export default async function handler(req, res) {
+  const INDEXNOW_KEY = 'f957624a77ca4beea15944d6ee307b97';
+
+  res.setHeader('Content-Type', 'text/plain');
+  return res.status(200).send(INDEXNOW_KEY);
+}
 
